e6752b36e63692dca36229ca7192b7c2643a7207
Asad-Saddiqui <asadsaddiqui101@gmail.com>
2024-06-11 21:19:24 -0700

    updated Code today 12/6/2024

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,92 +7,95 @@ function LandingPage() {
             <div className='homepage'>
 
 
-                <div className='LandingPage'>
-                    <div className='LandingPage-content'>
-                        <div className='navbar'>
-                            <div className='app-name'>
-                                Investor App
-                            </div>
-                            <div className='navbar-item'>
-                                <div className='active'>Home</div>
-                                <div className='links'>About App</div>
-                                <div className='links'>FAQs</div>
-                                <div className='links'>Preview</div>
-                            </div>
-                            <button>
-                                Download
-                            </button>
-                        </div>
-                        <div className='landing-main'>
-                            <div className="landing-main-content">
-                                <div className='landing-main-heading-1'>
-                                    Revolutionizing social<br />  media for
-                                </div>
-                                <div className='landing-main-heading-2'>
-                                    <span className='span-one'>Entrepreneurs and </span><br></br><span className='span-one'>Investors</span>
-                                </div>
-                                <div className='landing-main-paragraph'>
-                                    <p>Turn your vision into reality. Join the exclusive network connecting ambitious entrepreneurs with the investors.</p>
-                                </div>
-                                <div className='landing-btn'>
-                                    <button className='download-btn'>
-                                        Download Now
-                                    </button>
-                                    <button className='See-in-Action'>
-                                        See in Action
-                                    </button>
-                                </div>
-                            </div>
-                            <div className='landing-main-social'>
-                                <div className='landing-main-social-image'>
-                                    <img src="/854shots-so-1@2x.png" alt="" />
-                                </div>
-                                <div className='landing-main-social-black-image'>
-                                    <img src="/total users.png" alt="" />
-                                </div>
-                                <div className='social-content'>
-                                    <div className='social-one'>
-                                        <div className='headding'>
-                                            Invistore
-                                        </div>
-                                        <div className='count'>
-                                            51K
-                                        </div>
-                                        <div className='lines'>
-                                            <div className='line'></div>
-                                        </div>
-
-                                    </div>
-                                    <div className='social-one'>
-                                        <div className='headding'>
-                                            Entrepreneurs
-                                        </div>
-                                        <div className='count'>
-                                            81K
-                                        </div>
-                                        <div className='lines'>
-                                            <div className='line1'></div>
-                                        </div>
-
-                                    </div>
-                                    <div className='social-one'>
-                                        <div className='headding'>
-                                            Viewers
-                                        </div>
-                                        <div className='count'>
-                                            490
-                                        </div>
-                                        <div className='lines'>
-                                            <div className='line2'></div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div className="rectangle-div" >
+                    <div className='image'>
+                        <img src='/rectangle-3.png' alt="" />
                     </div>
-                    <div className='landing-image'>
-                        <img src="/rectangle-3.svg" alt="" />
+                    <div className="component-parent">
+
+                        <div className="frame-parent">
+                            <div className="investor-app-parent">
+                                <b className="investor-app">Investor App</b>
+                                <div className="home-parent">
+                                    <b className="home">Home</b>
+                                    <div className="investor-app">About App</div>
+                                    <div className="investor-app">FAQs</div>
+                                    <div className="investor-app">Preview</div>
+                                </div>
+                                <div className="frame-wrapper">
+                                    <div className="download-wrapper">
+                                        <b className="investor-app">Download</b>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="frame-group">
+                                <div className="frame-container">
+                                    <div className="frame-container">
+                                        <div className="revolutionizing-social-media-container">
+                                            <span>{`Revolutionizing social media for `}</span>
+                                            <span className="entrepreneurs-and-investors">
+                                                Entrepreneurs and Investors
+                                            </span>
+                                        </div>
+                                        <div className="frame-child" />
+                                        <div className="turn-your-vision">
+                                            Turn your vision into reality. Join the exclusive network
+                                            connecting ambitious entrepreneurs with the investors.
+                                        </div>
+                                        <div className="frame-item" />
+                                        <div className="frame-div">
+                                            <div className="download-parent">
+                                                <img className="download-icon" alt="" src="/download.svg" />
+                                                <b className="investor-app">Download Now</b>
+                                            </div>
+                                            <div className="play-circle-parent">
+                                                <img className="download-icon" alt="" src="/playcircle.svg" />
+                                                <b className="investor-app">See in Action</b>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="group-wrapper">
+                                    <div className="group-div">
+                                        <div className="shots-so-1-wrapper">
+                                            <img
+                                                className="shots-so-1-icon"
+                                                alt=""
+                                                src="/854shots-so-1@2x.png"
+                                            />
+                                        </div>
+                                        <div className="total-users">
+                                            <div className="bg" />
+                                            <img className="component-1-icon" alt="" src="/component-1@2x.png" />
+                                            <b className="connecting">Connecting</b>
+                                            <div className="people">People</div>
+                                            <img className="circle-icon" alt="" src="/circle.svg" />
+                                        </div>
+                                        <div className="rating-board">
+                                            <div className="rating-board-child" />
+                                            <div className="div">
+                                                <div className="investors">Investors</div>
+                                                <div className="k">51K</div>
+                                                <div className="child" />
+                                                <div className="item" />
+                                            </div>
+                                            <div className="div1">
+                                                <div className="viewers">Viewers</div>
+                                                <div className="div2">490</div>
+                                                <div className="child" />
+                                                <div className="child1" />
+                                            </div>
+                                            <div className="div3">
+                                                <div className="entrepreneurs">Entrepreneurs</div>
+                                                <div className="k">81K</div>
+                                                <div className="child" />
+                                                <div className="child3" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='section-two'>
@@ -127,7 +130,7 @@ function LandingPage() {
                         </div>
                         <div className='section-three-box-color'>
                             <div className='icon'>
-                               
+
                                 <img src="/read-icon4.svg" alt="" />
                             </div>
                             <div className='paragraph'>
